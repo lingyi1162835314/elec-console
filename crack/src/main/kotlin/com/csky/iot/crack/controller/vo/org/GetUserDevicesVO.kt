1b4a64e3f904bd383ae0e2cb389cf1e5ef060493
yeah <923792729@qq.com>
2018-03-14 14:35:41 +0800

    delete

--- crack/src/main/kotlin/com/csky/iot/crack/controller/vo/org/GetUserDevicesVO.kt
+++
@@ -1,8 +0,0 @@
-package com.csky.iot.crack.controller.vo.org
-
-import javax.validation.constraints.NotNull
-
-class GetUserDevicesVO {
-    @NotNull(message = "userId不能为空")
-    var userId: Long? = null
-}
--- crack/src/main/kotlin/com/csky/iot/crack/service/deviceShadow/DeviceOtaAckEnum.kt
+++
@@ -1,7 +0,0 @@
-package com.csky.iot.crack.service.deviceShadow
-
-enum class DeviceOtaAckEnum(var value: String) {
-    SUCCESS("OTA success"),
-    FAILED("OTA failed"),
-    ERROR("OTA no version")
-}
